90193c01962d38c3605bf3416c0e2f206015d7de
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2015-04-20 21:43:20 +0200

    travis

--- writer_test.go
+++ writer_test.go
@@ -3,6 +3,7 @@ package rat
 import (
 	"archive/tar"
 	"bytes"
+	"os"
 	"testing"
 	"time"
 
@@ -45,6 +46,52 @@ func (s *TestSuite) TestWriter(c *C) {
 	c.Assert(w.Close(), IsNil)
 
 	r, err := NewReader(bytes.NewReader(f.Bytes()))
+	c.Assert(err, IsNil)
+
+	d, err := r.ReadFile("foo.txt")
+	c.Assert(err, IsNil)
+	c.Assert(string(d), Equals, "foo")
+
+	d, err = r.ReadFile("bar.txt")
+	c.Assert(err, IsNil)
+	c.Assert(string(d), Equals, "bar")
+}
+
+func (s *TestSuite) TestWriterRealFile(c *C) {
+	f, err := os.Create("/tmp/foo.tar")
+	c.Assert(err, IsNil)
+
+	w := NewWriter(f)
+
+	h := &tar.Header{
+		Name:    "foo.txt",
+		Mode:    0640,
+		Size:    3,
+		ModTime: time.Unix(1244428340, 0),
+	}
+
+	c.Assert(w.WriteHeader(h), IsNil)
+	n, err := w.Write([]byte("foo"))
+	c.Assert(n, Equals, 3)
+	c.Assert(err, IsNil)
+
+	c.Assert(w.WriteHeader(&tar.Header{
+		Name:    "bar.txt",
+		Mode:    0640,
+		Size:    3,
+		ModTime: time.Unix(1244428340, 0),
+	}), IsNil)
+
+	n, err = w.Write([]byte("bar"))
+	c.Assert(n, Equals, 3)
+	c.Assert(err, IsNil)
+
+	c.Assert(w.Close(), IsNil)
+
+	f, err = os.Open("/tmp/foo.tar")
+	c.Assert(err, IsNil)
+
+	r, err := NewReader(f)
 	c.Assert(err, IsNil)
 
 	d, err := r.ReadFile("foo.txt")
